19f50792c08dc6d8c0af4f8b7f4ee9a14f008dd0
Neo <scottfinamore@gmail.com>
2023-11-18 00:04:35 -0500

    working state: rendered list items on page, currently styling components

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import './Business.css';
+import React from "react";
+import "./Business.css";
 
 const Business = () => {
- const businessData = {
-     imgSource: 'fakeimageplace.codecademy.com',
-     name: 'restaurant name',
-     address: '1234 Florida Ave.',
-     city: 'Tampa',
-     state: 'FL',
-     zipcode: '44512',
-     category: 'Italian',
-     rating: 4.5,
-     review_count: 86
-    }
-    
-    
-    return (
-    <div className='business'>
-        <div className='businessImage'>
-            <img src={businessData.imgSource} alt={businessData.name} />
-        </div>
-        <h2 className='businessName'>{businessData.name}</h2>
-        <div className='businessInfo'>
-          <div className='businessAddress'>
-            <p>{businessData.address}</p>
-            <p className='businessCityStateZip'>{`${businessData.city}, ${businessData.state} ${businessData.zipcode}`}</p>
-          </div>
-          <div className='businessDetails'>
-            <h3 className='businessCategory'>{businessData.category}</h3>
-            <h3 className='businessRating'>{businessData.rating}</h3>
-            <p className='businessReviewCount'>{businessData.review_count}</p>
-          </div>
-        </div>
-    </div>
+  const businessData = {
+    imgSource:
+      "https://t3.ftcdn.net/jpg/00/27/57/96/360_F_27579652_tM7V4fZBBw8RLmZo0Bi8WhtO2EosTRFD.jpg",
+    name: "restaurant name",
+    address: "1234 Florida Ave.",
+    city: "Tampa",
+    state: "FL",
+    zipcode: "44512",
+    category: "Italian",
+    rating: 4.5,
+    review_count: 86,
+  };
 
-    )
-    }
+  return (
+    <div className="business">
+      <div className="businessImage">
+        <img src={businessData.imgSource} alt={businessData.name} />
+      </div>
+      <h2 className="businessName">{businessData.name}</h2>
+      <div className="businessInfo">
+        <div className="businessAddress">
+          <p>{businessData.address}</p>
+          <p className="businessCityStateZip">{`${businessData.city}, ${businessData.state} ${businessData.zipcode}`}</p>
+        </div>
+        <div className="businessDetails">
+          <h3 className="businessCategory">{businessData.category}</h3>
+          <h3 className="businessRating">{businessData.rating}</h3>
+          <p className="businessReviewCount">{businessData.review_count}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Business;
--- src/Components/BusinessList.js
+++ src/Components/BusinessList.js
@@ -1,77 +1,80 @@
-import React from 'react';
-import Business from './Business.js';
+import React from "react";
+import Business from "./Business.js";
+import "./BusinessList.css";
 
 const BusinessList = () => {
-    const businesses = [
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name1',
-        address: '1 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+  const businesses = [
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name1",
+      address: "1 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name2',
-        address: '2 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name2",
+      address: "2 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name3',
-        address: '3 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name3",
+      address: "3 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name4',
-        address: '4 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name4",
+      address: "4 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name5',
-        address: '5 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name5",
+      address: "5 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-        {
-        imgSource: 'fakeimageplace.codecademy.com',
-        name: 'restaurant name6',
-        address: '6 Florida Ave.',
-        city: 'Tampa',
-        state: 'FL',
-        zipcode: '44512',
-        rating: 4.5,
-        review_count: 80
+    {
+      imgSource: "fakeimageplace.codecademy.com",
+      name: "restaurant name6",
+      address: "6 Florida Ave.",
+      city: "Tampa",
+      state: "FL",
+      zipcode: "44512",
+      rating: 4.5,
+      review_count: 80,
     },
-    ]
+  ];
 
-    return (
-        <div className='businessList'>
-            {businesses.map((business, index) => (
-            <Business key={index} business={business} />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="businessListWrapper">
+      <div className="businessList">
+        {businesses.map((business, index) => (
+          <Business key={index} businessData={business} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default BusinessList;
